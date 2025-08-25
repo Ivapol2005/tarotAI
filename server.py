2f35449c18ab02316ec75d56fd223b5ff5d5f894
Ivapol2005 <ivapol@outlook.com>
2025-08-25 22:43:25 +0300

    now user can make his own message

--- server.py
+++ server.py
@@ -1,17 +1,24 @@
+# server.py
+
 import http.server
 import ssl
 
 PORT = 8000
 
-# Створюємо сервер
-server_address = ('', PORT)
-httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+class CORSHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        super().end_headers()
 
-# Обгортаємо сокет SSL
-httpd.socket = ssl.wrap_socket(httpd.socket,
-                               keyfile="key.pem",
-                               certfile="cert.pem",
-                               server_side=True)
+server_address = ('', PORT)
+httpd = http.server.HTTPServer(server_address, CORSHTTPRequestHandler)
+
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
 print(f"Serving HTTPS on https://localhost:{PORT}")
 httpd.serve_forever()
